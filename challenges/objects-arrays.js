64a63c4a2ed69bdc078c7975871a709c85298dd5
dipeshkoirala <dipeshkoirala@gmail.com>
2020-03-22 20:41:04 -0400

    zooAnimal name to lowerCase()

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -238,6 +238,7 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 */
 
 const lowCaseAnimalNames = [];
+zooAnimals.map(a => lowCaseAnimalNames.push(a.animal_name.toLowerCase()));
 console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
